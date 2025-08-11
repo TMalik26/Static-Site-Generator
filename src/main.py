4e674865d9020dc7712e4e356619226e3ed92ca6
TMalik26 <maliktetianamykolaivna@gmail.com>
2025-08-11 16:57:02 +0300

    Set output folder to docs as required by GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from textnode import TextType, TextNode, copy_static, generate_page, generate_pages_recursive
+import sys
+
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
 
 def main():
-    copy_static("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
-    text_node_1 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node_1)
+    copy_static("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -281,7 +281,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("H1 header doesn't exsist in this file")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r', encoding='utf-8') as f:
         markdown_content = f.read()
@@ -289,13 +289,13 @@ def generate_page(from_path, template_path, dest_path):
         template_content = f.read()
     html_content = markdown_to_html_node(markdown_content).to_html()
     title = extract_title(markdown_content)
-    html_page = template_content.replace("{{ Title }}", title)
-    html_page = html_page.replace("{{ Content }}", html_content)
+    html_page = template_content.replace("{{ Title }}", title).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
+    html_page = html_page.replace("{{ Content }}", html_content).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w', encoding='utf-8') as f:
         f.write(html_page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     def recurse(current_path, current_dest_path):
         for entry in os.listdir(current_path):
             full_path = os.path.join(current_path, entry)
@@ -303,7 +303,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             if os.path.isfile(full_path):
                 if entry.endswith('.md'):
                     os.makedirs(current_dest_path, exist_ok=True)
-                    generate_page(full_path, template_path, dst_path)
+                    generate_page(full_path, template_path, dst_path, basepath)
                 
             else:
                 os.makedirs(dst_path, exist_ok=True)
